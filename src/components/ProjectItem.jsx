3ae259cc74b930fbacbdd3b90b7db1d8c69775fd
slawcka <slaviccc@gmail.com>
2019-01-18 20:06:00 +0200

    2.0

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -4,15 +4,16 @@ import React from 'react';
 
 const ProjectItem = (props) => {
     let item=props.current
-   
+    let resume=<i class="material-icons md-48"> play_circle_outline</i>
+    let pause=<i class="material-icons">pause_circle_outline</i>
         return (
-            <div className={item.active ? 'item active':'item false'}>
-                  <h1>{item.name} </h1>
-                <h2>{item.humanTime}</h2>
-                <button onClick={props.toggleTimer}>{item.active? 'pause':'resume'}</button>
+            <div className={item.active ? 'item-list__item active':'item-list__item false'}>
+                <h3>{item.name} </h3>
+                <p>{item.humanTime}</p>
+                <button onClick= {props.toggleTimer}>{item.active ? pause:resume} </button>
             </div>
         );
-    
+            
 }
 
 export default ProjectItem;
